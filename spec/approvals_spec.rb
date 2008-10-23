e71e62f9c68e1dd3fa0781e1a8d6368103ef2932
Dan Gilkerson <github@dangilkerson.com>
2008-10-22 17:07:29 -0700

    Refactored to be similar to the .Net and Java versions

--- spec/approvals_spec.rb
+++ spec/approvals_spec.rb
@@ -2,37 +2,30 @@ require File.join(File.dirname(__FILE__), "spec_helper")
 
 shared_examples_for "Shared Behaviors" do
   it "should approve shared behaviors" do
-    approve_text("shared behavior test")
+    approve("shared behavior test")
   end
 end
 
 describe "Approvals" do 
-  include Approvals
   it_should_behave_like "Shared Behaviors"
   
-  it "should pass" do 
-    are_files_same("passing.txt", "expected.txt" ).should == true
-  end
-
-  it "should fail" do
-    are_files_same("failing.txt", "expected.txt").should == false
-  end
-
-  it "should fail when doesn't exist" do
-    are_files_same("failng.txt", "non_existing.txt").should == false
-  end
-
-  it "should get name" do   
-    spec_name.should == "approvals_should_get_name"
+  it "should successfully approve text" do
+    approve("should be approved")
   end
   
-  it "should approve" do
-    approve_text("approve this")
+  it "should raise mismatch error if received does not match approved" do
+    register_reporter(CleanupReporter.new);
+    lambda { approve("should fail with mismatch") }.should raise_error(RuntimeError)
+  end
+  
+  it "should raise missing approval error if has not been approved yet" do
+    register_reporter(CleanupReporter.new);
+    lambda { approve("should fail with a missing exception") }.should raise_error(RuntimeError)
   end
   
   describe "Multiple describes" do
     it "should approve multiple describes" do
-      approve_text("multiple describes")
+      approve("multiple describes")
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,17 @@
 require 'rubygems'
 require 'spec'
 
-__DIR__ = File.join(File.dirname(__FILE__),  "..", "lib")
+__DIR__ = File.join(File.dirname(__FILE__))
+$LOAD_PATH.unshift __DIR__ unless
+  $LOAD_PATH.include?(__DIR__) ||
+  $LOAD_PATH.include?(File.expand_path(__DIR__))
+
+require 'cleanup_reporter'
+
+__DIR__ = File.join(__DIR__,  "..", "lib")
  
 $LOAD_PATH.unshift __DIR__ unless
   $LOAD_PATH.include?(__DIR__) ||
   $LOAD_PATH.include?(File.expand_path(__DIR__))
 
-require "approvals"
-
-include ApprovalTests
-
-Dir.chdir(File.dirname(__FILE__))
+require 'approval_tests'
