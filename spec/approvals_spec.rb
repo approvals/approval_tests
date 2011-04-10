1110788ee14db6b631f14983f88dcf0fc21b8820
Katrina Owen <katrina.owen@gmail.com>
2011-04-10 18:17:44 +0200

    Cleaning up whitespace. Safe to ignore.

--- spec/approvals_spec.rb
+++ spec/approvals_spec.rb
@@ -10,29 +10,29 @@ describe "Approvals" do
   after(:each) do
     Approvals.unregister_last_reporter()
   end
-  
+
   it_should_behave_like "Shared Behaviors"
-  
+
   it "should successfully approve text" do
     Approvals.approve("should be approved")
   end
-  
+
   it "should raise mismatch error if received does not match approved" do
     Approvals.reporter(CleanupReporter.new);
     lambda { Approvals.approve("should fail with mismatch") }.should raise_error(ApprovalError)
   end
-  
+
   it "should raise missing approval error if has not been approved yet" do
     Approvals.reporter(CleanupReporter.new);
     lambda { Approvals.approve("should fail with a missing ") }.should raise_error(ApprovalError)
   end  
-  
+
   describe "Multiple describes" do
     it "should approve multiple describes" do
       Approvals.approve("multiple describes")
     end
   end
-  
+
   describe "lists" do
     it "should write arrays" do
       Approvals.approve_list("name",["tom","dick","harry"])
@@ -44,32 +44,28 @@ describe "Approvals" do
       Approvals.approve_map(:name=> "harry", :age => 25, :sex => "male")
     end
   end
-  
+
   describe "xml" do
     it "should be pretty" do
       Approvals.approve_xml('<?xml version="1.0" encoding="ISO-8859-1"?><data><a><b>Bee</b><c>Sea</c></a></data>')
     end
   end
-  
+
   describe "DiffReporter" do
     it "should launch" do
       base = "approval_tests/spec/"
-   #   DiffReporter.instance.report("approval_tests/spec/a.txt", "approval_tests/spec/b.txt")    
-   end 
-     it "should create and launch" do
-        base = "approval_tests/spec/"
-        File.delete("#{base}c.txt") if File.exists?("#{base}c.txt")
-   #     DiffReporter.instance.report("#{base}c.txt", "#{base}a.txt")    
-      end
+      #   DiffReporter.instance.report("approval_tests/spec/a.txt", "approval_tests/spec/b.txt")    
+    end 
+    it "should create and launch" do
+      base = "approval_tests/spec/"
+      File.delete("#{base}c.txt") if File.exists?("#{base}c.txt")
+      #     DiffReporter.instance.report("#{base}c.txt", "#{base}a.txt")    
+    end
   end
-    
 end
-
-
 
 describe "Approvals", "using approval blocks" do
   approve "the last statement" do
     "approve this"
   end
-  
 end
